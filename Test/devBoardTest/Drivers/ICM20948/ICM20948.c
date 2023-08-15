b3b52451535eb3b105eda6b217e55816c18958fd
chooboy <chooboy05@gmail.com>
2023-08-15 13:42:16 -0400

    Gyro and Accel get functions
    From looking at datasheets and existing sensor code, I believe these functions should be able to get the values for the accelerometer and gyroscope for the ICM20948 device

--- Test/devBoardTest/Drivers/ICM20948/ICM20948.c
+++ Test/devBoardTest/Drivers/ICM20948/ICM20948.c
@@ -25,3 +25,93 @@ int32_t ICM20948_wakeup(ICM20948_dev * dev) {
 	return HAL_OK;
 
 }
+
+int32_t ICM20948_get_x_accel(ICM20948_dev * dev, float * accel) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_XOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_XOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*accel = ACCEL_XOUT/Accel_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*accel = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
+
+int32_t ICM20948_get_y_accel(ICM20948_dev * dev, float * accel) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_YOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_YOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*accel = ACCEL_YOUT/Accel_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*accel = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
+
+int32_t ICM20948_get_z_accel(ICM20948_dev * dev, float * accel) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_ZOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_ACCEL_ZOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*accel = ACCEL_ZOUT/Accel_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*accel = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
+
+int32_t ICM20948_get_x_gyro(ICM20948_dev * dev, float * gyro) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_XOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_XOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*gyro = GYRO_XOUT/Gyro_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*gyro = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
+
+int32_t ICM20948_get_y_gyro(ICM20948_dev * dev, float * gyro) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_YOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_YOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*gyro = GYRO_YOUT/Gyro_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*gyro = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
+
+int32_t ICM20948_get_z_gyro(ICM20948_dev * dev, float * gyro) {
+	uint8_t data = 0;
+	if	(dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_ZOUT_H, 1, (uint8_t *)&new_val, 1) == HAL_OK
+		&& dev->bus->read(dev->addr, ICM20948_REG_B0_GYRO_ZOUT_L, 1, (uint8_t *)&new_val, 1) == HAL_OK)
+	{
+		*gyro = GYRO_ZOUT/Gyro_Sensitivity;
+		return HAL_OK;
+	}
+	else {
+		*gyro = 0;
+		return HAL_ERROR;
+	}
+	return HAL_ERROR;
+}
